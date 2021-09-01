a553218fded12088b705d7af021579c4c279e546
Marco Ris <info@risdesign.ch>
2021-09-01 21:15:24 +0200

    Update install.sh

--- lamp/install.sh
+++ lamp/install.sh
@@ -12,6 +12,6 @@ if [[ "$ANSWER" != "${ANSWER#[Yy]}" ]]; then
     sudo apt-get update && sudo apt-get install vsftpd apache2 php mariadb-server-10.0 -y && sudo cp vsftpd.conf /etc/vsftpd.conf && sudo mkdir -p /home/pi/FTP && sudo chmod a-w /home/pi/FTP && sudo service vsftpd restart && sudo cp cmdline.txt /boot/cmdline.txt;
 fi
 
-if [[ $IP -ne 0 ]]; then
+if [[ "$IP" != 0 ]]; then
   sudo echo "ip=${IP}" >> /boot/cmdline.txt
 fi
