3f52805c981956f930d09de07b57a550ba0b0611
Marco Ris <info@risdesign.ch>
2021-09-01 21:11:42 +0200

    Update install.sh

--- lamp/install.sh
+++ lamp/install.sh
@@ -8,7 +8,7 @@ read IP
 
 sudo apt-get update
 
-if [ "$ANSWER" != "${ANSWER#[Yy]}" ]; then
+if [[ "$ANSWER" != "${ANSWER#[Yy]}" ]]; then
     sudo apt-get update && sudo apt-get install vsftpd apache2 php mariadb-server-10.0 -y && sudo cp vsftpd.conf /etc/vsftpd.conf && sudo mkdir -p /home/pi/FTP && sudo chmod a-w /home/pi/FTP && sudo service vsftpd restart && sudo cp cmdline.txt /boot/cmdline.txt;
 fi
 
